504f6868233390e71e6b0b18469b2807d7f155ae
Tech Yoghurt <nikhilvaidyar1997@gmail.com>
2020-04-10 14:36:03 +0530

    Initial Setup

---
+++ backend/dummy.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        "logo": "http://xyz.com",
+        "name": "Let's Endorse",
+        "registration_date": "12/12/2001",
+        "foreign_funding_received": false,
+        "email_id": "dummy_endorse@gmail.com",
+        "address": [
+            {
+                "line_1": "xyz flat 233",
+                "line_2": "JP Nagar",
+                "city": "Bengaluru",
+                "district": "Nagar",
+                "state": "Karnataka",
+                "pincode": "560078"
+            }
+        ],
+        "claimed": false,
+        "unclaimed": true
+    }
+]
---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import SearchBar from './components/SearchBar/SearchBar'
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
