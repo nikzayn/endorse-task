e2a77b466dd258bc06f5a8db9f76b1508b4cc6e1
Tech Yoghurt <nikhilvaidyar1997@gmail.com>
2020-04-11 17:57:50 +0530

    Dummy Data --update

---
+++ backend/src/dummy.js
@@ -0,0 +1,83 @@
+module.exports = [
+    {
+        "logo": "https://www.logo-company.in/logo/2.jpg",
+        "name": "CRY",
+        "registration_date": "12-12-2001",
+        "funding": true,
+        "email_id": "dummy_endorse@gmail.com",
+        "address": {
+            "line_1": "xyz flat 233",
+            "line_2": "JP Nagar",
+            "city": "Bengaluru",
+            "district": "Bengaluru Urban",
+            "state": "Karnataka",
+            "pincode": "560078"
+        },
+        "claimed": false
+    },
+    {
+        "logo": "https://image.shutterstock.com/image-vector/ngo-logo-children-foundation-company-260nw-1516420154.jpg",
+        "name": "Helping kids",
+        "registration_date": "12-12-2001",
+        "funding": true,
+        "email_id": "dummy_endorse@gmail.com",
+        "address": {
+            "line_1": "S1 ThoughtCell",
+            "line_2": "JP Nagar",
+            "city": "Chennai",
+            "district": "Bengaluru Urban",
+            "state": "Tamil Nadu",
+            "pincode": "560078"
+        },
+        "claimed": false
+    },
+    {
+        "logo": "https://thelogocompany.net/wp-content/uploads/2016/10/people-helping-people.jpg",
+        "name": "Aisha Foundation",
+        "registration_date": "12-12-2001",
+        "funding": false,
+        "email_id": "dummy_endorse@gmail.com",
+        "address":
+        {
+            "line_1": "21 Main Road",
+            "line_2": "Building No. 4",
+            "city": "Thrissur",
+            "district": "Thrissur",
+            "state": "Kerala",
+            "pincode": "560078"
+        },
+        "claimed": false
+    },
+    {
+        "logo": "https://www.logodesignteam.com/images/portfolio-images/non-profit-organisations-logo-design/ngo-non-profit-organisations-logo-design1.jpg",
+        "name": "Girl Foundation",
+        "registration_date": "12-12-2001",
+        "funding": false,
+        "email_id": "dummy_endorse@gmail.com",
+        "address": {
+            "line_1": "Model Town",
+            "line_2": "JP Nagar",
+            "city": "Mumbai",
+            "district": "Mumbai",
+            "state": "Maharashtra",
+            "pincode": "560078"
+        },
+        "claimed": true
+    },
+    {
+        "logo": "https://image.shutterstock.com/image-vector/child-care-center-logo-design-260nw-400260754.jpg",
+        "name": "SGBS Trust",
+        "registration_date": "12-12-2001",
+        "funding": false,
+        "email_id": "dummy_endorse@gmail.com",
+        "address": {
+            "line_1": "S1 ThoughtCell",
+            "line_2": "JP Nagar",
+            "city": "Goa Velha",
+            "district": "Nagar",
+            "state": "Goa",
+            "pincode": "560078"
+        },
+        "claimed": false
+    }
+]
